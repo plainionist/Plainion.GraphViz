acc678f7b3dc6975a0f7efacc63de269e473b6ac
ronin4net <stanley751@gmx.net>
2016-01-10 09:34:14 +0100

    packager version fixed

--- build/create-package.cmd
+++ build/create-package.cmd
@@ -4,7 +4,7 @@ setlocal
 set PATH=%PATH%;C:\Program Files (x86)\MSBuild\12.0\Bin
 set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
 
-msbuild /verbosity:normal /p:Version=1.3.0.0 packager.msbuild
+msbuild /verbosity:normal /p:Version=1.4.0.0 packager.msbuild
 
 pause
 		
