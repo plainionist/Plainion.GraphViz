c7789b9987d563363dece2afb60a64bf24fbbff7
ronin4net <stanley751@gmx.net>
2016-02-22 22:03:26 +0100

    version increased to 1.6.1

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyDescription( "Plainion.GraphViz" )]
 [assembly: AssemblyCopyright( "Copyright © 2015" )]
-[assembly: AssemblyVersion( "1.6.0.0" )]
+[assembly: AssemblyVersion( "1.6.1.0" )]
 [assembly: AssemblyProduct( "Plainion.GraphViz" )]
