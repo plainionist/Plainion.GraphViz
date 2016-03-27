3f534b829e9e40909bc8b7910fec5750283df470
ronin4net <stanley751@gmx.net>
2016-03-27 13:57:56 +0200

    version increased to 1.9

--- build/GlobalAssemblyInfo.cs
+++ build/GlobalAssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyDescription( "Plainion.GraphViz" )]
 [assembly: AssemblyCopyright( "Copyright © 2015" )]
-[assembly: AssemblyVersion( "1.8.0.0" )]
+[assembly: AssemblyVersion( "1.9.0.0" )]
 [assembly: AssemblyProduct( "Plainion.GraphViz" )]
