91838d6ec8e5266ec782e9d59ce6fb8575871021
ronin4net <stanley751@gmx.net>
2016-04-09 14:19:00 +0200

    AnalyzeBase.cs removed

--- src/Plainion.GraphViz.Modules.CodeInspection/Packaging/Services/PackageAnalyzer.cs
+++ src/Plainion.GraphViz.Modules.CodeInspection/Packaging/Services/PackageAnalyzer.cs
@@ -1,26 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
 using Plainion.GraphViz.Modules.CodeInspection.Packaging.Spec;
 
 namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
 {
-    class PackageAnalyzer : AnalyzeBase
+    class PackageAnalyzer 
     {
         private static string[] Colors = { "LightBlue", "LightGreen", "LightGray" };
 
-        private readonly Dictionary<string, List<Type>> myPackages = new Dictionary<string, List<Type>>();
+        private SystemPackaging myConfig;
+        private CancellationToken myCancellationToken;
+        private AssemblyLoader myAssemblyLoader;
+        private readonly Dictionary<string, List<Type>> myPackages;
+
+        public PackageAnalyzer()
+        {
+            myAssemblyLoader = new AssemblyLoader();
+            myPackages = new Dictionary<string, List<Type>>();
+        }
 
         public string PackageName { get; set; }
-
-        protected override void Load()
+        
+        public AnalysisDocument Execute( SystemPackaging config, CancellationToken cancellationToken )
         {
-            var relevantPackages = Config.Packages
+            myConfig = config;
+            myCancellationToken = cancellationToken;
+
+            Load();
+
+            Console.WriteLine("Analyzing ...");
+
+            var edges = Analyze()
+                .Distinct()
+                .ToList();
+
+            Console.WriteLine();
+
+            if (myAssemblyLoader.SkippedAssemblies.Any())
+            {
+                Console.WriteLine("Skipped assemblies:");
+                foreach (var asm in myAssemblyLoader.SkippedAssemblies)
+                {
+                    Console.WriteLine("  {0}", asm);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Building Graph ...");
+
+            return GenerateDocument(edges);
+        }
+
+        private void Load()
+        {
+            var relevantPackages = myConfig.Packages
                 .Where( p => string.IsNullOrEmpty( PackageName ) || p.Name.Equals( PackageName, StringComparison.OrdinalIgnoreCase ) );
 
             foreach( var package in relevantPackages )
             {
-                CancellationToken.ThrowIfCancellationRequested();
+                myCancellationToken.ThrowIfCancellationRequested();
 
                 myPackages[ package.Name ] = Load( package )
                     .SelectMany( asm => asm.GetTypes() )
@@ -28,9 +70,22 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
             }
         }
 
-        protected override Edge[] Analyze()
+        private IEnumerable<Assembly> Load( Package package )
         {
-            var relevantPackages = Config.Packages
+            Console.WriteLine("Assembly root {0}", Path.GetFullPath(myConfig.AssemblyRoot));
+            Console.WriteLine("Loading package {0}", package.Name);
+
+            return package.Includes
+                .SelectMany(i => Directory.GetFiles(myConfig.AssemblyRoot, i.Pattern))
+                .Where(file => !package.Excludes.Any(e => e.Matches(file)))
+                .Select(myAssemblyLoader.Load)
+                .Where(asm => asm != null)
+                .ToList();
+        }
+
+        private Edge[] Analyze()
+        {
+            var relevantPackages = myConfig.Packages
                 .Where( p => string.IsNullOrEmpty( PackageName ) || p.Name.Equals( PackageName, StringComparison.OrdinalIgnoreCase ) );
 
             return relevantPackages
@@ -42,7 +97,7 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
                     } )
                 )
                 .AsParallel()
-                .WithCancellation( CancellationToken )
+                .WithCancellation( myCancellationToken )
                 .SelectMany( e => Analyze( e.Package, e.Type ) )
                 .ToArray();
         }
@@ -51,13 +106,13 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
         {
             Console.Write( "." );
 
-            CancellationToken.ThrowIfCancellationRequested();
+            myCancellationToken.ThrowIfCancellationRequested();
 
             var focusedPackageTypes = string.IsNullOrEmpty( PackageName )
                 ? null
                 : myPackages.Single( p => p.Key.Equals( PackageName, StringComparison.OrdinalIgnoreCase ) ).Value;
 
-            return new Reflector( AssemblyLoader, type ).GetUsedTypes()
+            return new Reflector( myAssemblyLoader, type ).GetUsedTypes()
                 // if only one package is given we analyse the deps within the package - otherwise between the packages
                 .Where( edge => focusedPackageTypes != null ? focusedPackageTypes.Contains( edge.Target ) : IsForeignPackage( package, edge.Target ) )
                 .Select( edge => GraphUtils.Edge( edge ) )
@@ -69,7 +124,7 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
             return myPackages.Where( e => e.Key != package.Name ).Any( entry => entry.Value.Contains( dep ) );
         }
 
-        protected override AnalysisDocument GenerateDocument( IReadOnlyCollection<Edge> edges )
+        private AnalysisDocument GenerateDocument( IReadOnlyCollection<Edge> edges )
         {
             var doc = new AnalysisDocument();
 
@@ -80,9 +135,9 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
                 nodesWithEdgesIndex.Add( edge.Target );
             }
 
-            for( int i = 0; i < Config.Packages.Count; ++i )
+            for( int i = 0; i < myConfig.Packages.Count; ++i )
             {
-                var package = Config.Packages[ i ];
+                var package = myConfig.Packages[ i ];
 
                 if( !myPackages.ContainsKey( package.Name ) )
                 {
