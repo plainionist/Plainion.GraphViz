0bc1d54c74841d9087c52c3f01e616afc04dc05a
ronin4net <stanley751@gmx.net>
2016-04-09 13:28:58 +0200

    Handling of empty transformations fixed

--- src/Plainion.GraphViz/Presentation/TransformationModule.cs
+++ src/Plainion.GraphViz/Presentation/TransformationModule.cs
@@ -12,19 +12,19 @@ namespace Plainion.GraphViz.Presentation
         private IGraph myGraph;
         private List<IGraphTransformation> myTransformations;
 
-        public TransformationModule(GraphPresentation presentation)
+        public TransformationModule( GraphPresentation presentation )
         {
             myPresentation = presentation;
             myTransformations = new List<IGraphTransformation>();
 
             // Hint: we already add DynamicClusterTransformation here so that all fold operations are behind that and
             // that newly added nodes to a cluster are automatically folded (if folding is applied)
-            Add(new DynamicClusterTransformation());
+            Add( new DynamicClusterTransformation() );
         }
 
         public IGraph Graph
         {
-            get { return myTransformations.Count == 0 ? myPresentation.Graph : myGraph; }
+            get { return myGraph ?? myPresentation.Graph; }
         }
 
         public override IEnumerable<IGraphTransformation> Items
@@ -32,58 +32,58 @@ namespace Plainion.GraphViz.Presentation
             get { return myTransformations; }
         }
 
-        public void Add(IGraphTransformation transformation)
+        public void Add( IGraphTransformation transformation )
         {
-            myTransformations.Add(transformation);
+            myTransformations.Add( transformation );
 
             var notifyPropertyChanged = transformation as INotifyPropertyChanged;
-            if (notifyPropertyChanged != null)
+            if( notifyPropertyChanged != null )
             {
                 notifyPropertyChanged.PropertyChanged += OnTransformationChanged;
             }
 
             ApplyTransformations();
 
-            RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, transformation));
+            RaiseCollectionChanged( new NotifyCollectionChangedEventArgs( NotifyCollectionChangedAction.Add, transformation ) );
         }
 
-        private void OnTransformationChanged(object sender, PropertyChangedEventArgs e)
+        private void OnTransformationChanged( object sender, PropertyChangedEventArgs e )
         {
             ApplyTransformations();
         }
 
         private void ApplyTransformations()
         {
-            if (myPresentation.Graph == null)
+            if( myPresentation.Graph == null )
             {
                 // e.g. because we added initial DynamicClusterTransformation here
                 return;
             }
 
-            using (new Profile("ApplyTransformations"))
+            using( new Profile( "ApplyTransformations" ) )
             {
                 myGraph = myPresentation.Graph;
 
-                foreach (var transformation in myTransformations)
+                foreach( var transformation in myTransformations )
                 {
-                    myGraph = transformation.Transform(myGraph);
+                    myGraph = transformation.Transform( myGraph );
                 }
             }
         }
 
-        public void Remove(IGraphTransformation transformation)
+        public void Remove( IGraphTransformation transformation )
         {
-            myTransformations.Remove(transformation);
+            myTransformations.Remove( transformation );
 
             var notifyPropertyChanged = transformation as INotifyPropertyChanged;
-            if (notifyPropertyChanged != null)
+            if( notifyPropertyChanged != null )
             {
                 notifyPropertyChanged.PropertyChanged -= OnTransformationChanged;
             }
 
             ApplyTransformations();
 
-            RaiseCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, transformation));
+            RaiseCollectionChanged( new NotifyCollectionChangedEventArgs( NotifyCollectionChangedAction.Remove, transformation ) );
         }
     }
 }
