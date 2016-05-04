c19f10362be119fd1d73aa0c224bdd257e0167ce
ronin4net <stanley751@gmx.net>
2016-05-04 13:55:09 +0200

    more colorful packaging graph

--- src/Plainion.GraphViz.Modules.CodeInspection/Packaging/Services/PackageAnalyzer.cs
+++ src/Plainion.GraphViz.Modules.CodeInspection/Packaging/Services/PackageAnalyzer.cs
@@ -10,7 +10,8 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
 {
     class PackageAnalyzer
     {
-        private static string[] Colors = { "LightBlue", "LightGreen", "LightGray", "LightCoral", "Brown" };
+        private static string[] Colors = { "LightBlue", "LightGreen", "LightCoral", "Brown", "DarkTurquoise", "MediumAquamarine", "Orange", 
+                                             "LawnGreen","DarkKhaki","BurlyWood","SteelBlue","Goldenrod", "Tomato","Crimson","CadetBlue" };
 
         private SystemPackaging myConfig;
         private CancellationToken myCancellationToken;
@@ -36,14 +37,14 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
         /// </summary>
         public bool CreateClustersForNamespaces { get; set; }
 
-        public AnalysisDocument Execute( SystemPackaging config, CancellationToken cancellationToken )
+        public AnalysisDocument Execute(SystemPackaging config, CancellationToken cancellationToken)
         {
             myConfig = config;
             myCancellationToken = cancellationToken;
 
             myRelevantPackages = PackagesToAnalyze.Any()
                 ? myConfig.Packages
-                    .Where( p => PackagesToAnalyze.Any( name => p.Name.Equals( name, StringComparison.OrdinalIgnoreCase ) ) )
+                    .Where(p => PackagesToAnalyze.Any(name => p.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
                     .ToList()
                 : myConfig.Packages;
 
@@ -51,7 +52,7 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
 
             Load();
 
-            Console.WriteLine( "Analyzing ..." );
+            Console.WriteLine("Analyzing ...");
 
             var edges = Analyze()
                 .Distinct()
@@ -59,65 +60,65 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
 
             Console.WriteLine();
 
-            if( myAssemblyLoader.SkippedAssemblies.Any() )
+            if (myAssemblyLoader.SkippedAssemblies.Any())
             {
-                Console.WriteLine( "Skipped assemblies:" );
-                foreach( var asm in myAssemblyLoader.SkippedAssemblies )
+                Console.WriteLine("Skipped assemblies:");
+                foreach (var asm in myAssemblyLoader.SkippedAssemblies)
                 {
-                    Console.WriteLine( "  {0}", asm );
+                    Console.WriteLine("  {0}", asm);
                 }
                 Console.WriteLine();
             }
 
-            Console.WriteLine( "Building Graph ..." );
+            Console.WriteLine("Building Graph ...");
 
-            return GenerateDocument( edges );
+            return GenerateDocument(edges);
         }
 
         private void Load()
         {
-            foreach( var package in myRelevantPackages )
+            foreach (var package in myRelevantPackages)
             {
                 myCancellationToken.ThrowIfCancellationRequested();
 
-                myPackageToTypesMap[ package.Name ] = Load( package )
-                    .SelectMany( asm => asm.GetTypes() )
+                myPackageToTypesMap[package.Name] = Load(package)
+                    .SelectMany(asm => asm.GetTypes())
                     .ToList();
             }
         }
 
-        private IEnumerable<Assembly> Load( Package package )
+        private IEnumerable<Assembly> Load(Package package)
         {
-            Console.WriteLine( "Assembly root {0}", Path.GetFullPath( myConfig.AssemblyRoot ) );
-            Console.WriteLine( "Loading package {0}", package.Name );
+            Console.WriteLine("Assembly root {0}", Path.GetFullPath(myConfig.AssemblyRoot));
+            Console.WriteLine("Loading package {0}", package.Name);
 
             return package.Includes
-                .SelectMany( i => Directory.GetFiles( myConfig.AssemblyRoot, i.Pattern ) )
-                .Where( file => !package.Excludes.Any( e => e.Matches( file ) ) )
-                .Select( myAssemblyLoader.Load )
-                .Where( asm => asm != null )
+                .SelectMany(i => Directory.GetFiles(myConfig.AssemblyRoot, i.Pattern))
+                .Where(file => !package.Excludes.Any(e => e.Matches(file)))
+                .Select(myAssemblyLoader.Load)
+                .Where(asm => asm != null)
                 .ToList();
         }
 
         private Edge[] Analyze()
         {
             return myRelevantPackages
-                .SelectMany( p => myPackageToTypesMap[ p.Name ]
-                    .Select( t => new
+                .SelectMany(p => myPackageToTypesMap[p.Name]
+                    .Select(t => new
                     {
                         Package = p,
                         Type = t
-                    } )
+                    })
                 )
                 .AsParallel()
-                .WithCancellation( myCancellationToken )
-                .SelectMany( e => Analyze( e.Package, e.Type ) )
+                .WithCancellation(myCancellationToken)
+                .SelectMany(e => Analyze(e.Package, e.Type))
                 .ToArray();
         }
 
-        private IEnumerable<Edge> Analyze( Package package, Type type )
+        private IEnumerable<Edge> Analyze(Package package, Type type)
         {
-            Console.Write( "." );
+            Console.Write(".");
 
             myCancellationToken.ThrowIfCancellationRequested();
 
@@ -125,93 +126,93 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
                 ? null
                 : myPackageToTypesMap.Single().Value;
 
-            return new Reflector( myAssemblyLoader, type ).GetUsedTypes()
+            return new Reflector(myAssemblyLoader, type).GetUsedTypes()
                 // if only one package is given we analyse the deps within the package - otherwise between the packages
-                .Where( edge => focusedPackageTypes != null ? focusedPackageTypes.Contains( edge.Target ) : IsForeignPackage( package, edge.Target ) )
-                .Select( edge => GraphUtils.Edge( edge ) )
-                .Where( edge => edge.Source != edge.Target );
+                .Where(edge => focusedPackageTypes != null ? focusedPackageTypes.Contains(edge.Target) : IsForeignPackage(package, edge.Target))
+                .Select(edge => GraphUtils.Edge(edge))
+                .Where(edge => edge.Source != edge.Target);
         }
 
-        private bool IsForeignPackage( Package package, Type dep )
+        private bool IsForeignPackage(Package package, Type dep)
         {
-            return myPackageToTypesMap.Where( e => e.Key != package.Name ).Any( entry => entry.Value.Contains( dep ) );
+            return myPackageToTypesMap.Where(e => e.Key != package.Name).Any(entry => entry.Value.Contains(dep));
         }
 
-        private AnalysisDocument GenerateDocument( IReadOnlyCollection<Edge> edges )
+        private AnalysisDocument GenerateDocument(IReadOnlyCollection<Edge> edges)
         {
             var doc = new AnalysisDocument();
 
             var nodesWithEdgesIndex = new HashSet<Type>();
-            if( UsedTypesOnly )
+            if (UsedTypesOnly)
             {
-                foreach( var edge in edges )
+                foreach (var edge in edges)
                 {
-                    nodesWithEdgesIndex.Add( edge.Source );
-                    nodesWithEdgesIndex.Add( edge.Target );
+                    nodesWithEdgesIndex.Add(edge.Source);
+                    nodesWithEdgesIndex.Add(edge.Target);
                 }
             }
 
             var relevantNotesWithCluster = myPackageToTypesMap
-                .Select( e => new
+                .Select(e => new
                 {
-                    Package = myRelevantPackages.Single( p => p.Name == e.Key ),
+                    Package = myRelevantPackages.Single(p => p.Name == e.Key),
                     Types = e.Value
-                } )
-                .SelectMany( ( e, idx ) => e.Types
-                    .Select( t => new
+                })
+                .SelectMany((e, idx) => e.Types
+                    .Select(t => new
                     {
                         Type = t,
                         Package = e.Package,
                         PackageIndex = idx
-                    } ) )
+                    }))
                 .AsParallel()
-                .Where( e => !UsedTypesOnly || nodesWithEdgesIndex.Contains( e.Type ) )
-                .Select( e => new
+                .Where(e => !UsedTypesOnly || nodesWithEdgesIndex.Contains(e.Type))
+                .Select(e => new
                 {
-                    Node = GraphUtils.Node( e.Type ),
-                    Cluster = GetCluster( e.Package, e.Type ),
+                    Node = GraphUtils.Node(e.Type),
+                    Cluster = GetCluster(e.Package, e.Type),
                     PackageIndex = e.PackageIndex
-                } );
+                });
 
-            foreach( var entry in relevantNotesWithCluster )
+            foreach (var entry in relevantNotesWithCluster)
             {
-                doc.Add( entry.Node );
+                doc.Add(entry.Node);
 
-                if( entry.Cluster != null )
+                if (entry.Cluster != null)
                 {
-                    doc.AddToCluster( entry.Node, entry.Cluster );
+                    doc.AddToCluster(entry.Node, entry.Cluster);
                 }
 
-                if( myPackageToTypesMap.Count > 1 )
+                if (myPackageToTypesMap.Count > 1)
                 {
                     // color coding of nodes we only need if multiple packages were analyzed
-                    doc.AddNodeColor( entry.Node, Colors[ entry.PackageIndex % Colors.Length ] );
+                    doc.AddNodeColor(entry.Node, Colors[entry.PackageIndex % Colors.Length]);
                 }
             }
 
-            foreach( var edge in edges )
+            foreach (var edge in edges)
             {
-                doc.Add( edge );
+                doc.Add(edge);
 
-                var color = GetEdgeColor( edge );
-                if( color != null )
+                var color = GetEdgeColor(edge);
+                if (color != null)
                 {
-                    doc.AddEdgeColor( edge, color );
+                    doc.AddEdgeColor(edge, color);
                 }
             }
 
             return doc;
         }
 
-        private string GetCluster( Package package, Type type )
+        private string GetCluster(Package package, Type type)
         {
-            var cluster = package.Clusters.FirstOrDefault( c => c.Matches( type.FullName ) );
-            if( cluster != null )
+            var cluster = package.Clusters.FirstOrDefault(c => c.Matches(type.FullName));
+            if (cluster != null)
             {
                 return cluster.Name;
             }
 
-            if( CreateClustersForNamespaces )
+            if (CreateClustersForNamespaces)
             {
                 return type.Namespace;
             }
@@ -219,13 +220,13 @@ namespace Plainion.GraphViz.Modules.CodeInspection.Packaging.Services
             return null;
         }
 
-        private static string GetEdgeColor( Edge edge )
+        private static string GetEdgeColor(Edge edge)
         {
-            if( edge.EdgeType == EdgeType.DerivesFrom || edge.EdgeType == EdgeType.Implements )
+            if (edge.EdgeType == EdgeType.DerivesFrom || edge.EdgeType == EdgeType.Implements)
             {
                 return "Blue";
             }
-            else if( edge.EdgeType != EdgeType.Calls )
+            else if (edge.EdgeType != EdgeType.Calls)
             {
                 return "Gray";
             }
