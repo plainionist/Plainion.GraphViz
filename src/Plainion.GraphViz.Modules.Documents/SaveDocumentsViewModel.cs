caf2c61dbaff46254a532633dfa3187ea99a6d0d
ronin4net <stanley751@gmx.net>
2016-08-11 07:18:30 +0200

    Save button now saves visible graph instead of full graph

--- src/Plainion.GraphViz.Modules.Documents/SaveDocumentsViewModel.cs
+++ src/Plainion.GraphViz.Modules.Documents/SaveDocumentsViewModel.cs
@@ -16,7 +16,7 @@ using Plainion.GraphViz.Dot;
 
 namespace Plainion.GraphViz.Modules.Documents
 {
-    [Export(typeof(SaveDocumentsViewModel))]
+    [Export( typeof( SaveDocumentsViewModel ) )]
     public class SaveDocumentsViewModel : ViewModelBase
     {
         [Import]
@@ -27,7 +27,7 @@ namespace Plainion.GraphViz.Modules.Documents
 
         public SaveDocumentsViewModel()
         {
-            SaveDocumentCommand = new DelegateCommand(OnSave, CanSave);
+            SaveDocumentCommand = new DelegateCommand( OnSave, CanSave );
             SaveFileRequest = new InteractionRequest<SaveFileDialogNotification>();
         }
 
@@ -46,61 +46,62 @@ namespace Plainion.GraphViz.Modules.Documents
             notification.FilterIndex = 0;
             notification.DefaultExt = ".dot";
 
-            SaveFileRequest.Raise(notification,
+            SaveFileRequest.Raise( notification,
                 n =>
                 {
-                    if (n.Confirmed)
+                    if( n.Confirmed )
                     {
-                        Save(n.FileName);
+                        Save( n.FileName );
                     }
-                });
+                } );
         }
 
         public InteractionRequest<SaveFileDialogNotification> SaveFileRequest { get; private set; }
 
-        private void Save(string path)
+        private void Save( string path )
         {
-            if (Path.GetExtension(path).Equals(".dgml", StringComparison.OrdinalIgnoreCase))
+            if( Path.GetExtension( path ).Equals( ".dgml", StringComparison.OrdinalIgnoreCase ) )
             {
-                SaveAsDgml(path);
+                SaveAsDgml( path );
             }
             else
             {
-                SaveAsDot(path);
+                SaveAsDot( path );
             }
         }
 
-        private void SaveAsDgml(string path)
+        private void SaveAsDgml( string path )
         {
             var captionModule = Model.Presentation.GetModule<CaptionModule>();
             Func<Node, string> GetNodeCaption = node =>
                 {
-                    var caption = captionModule.Get(node.Id);
-                    if (caption == null)
+                    var caption = captionModule.Get( node.Id );
+                    if( caption == null )
                     {
                         return node.Id;
                     }
                     return caption.Label != null ? caption.Label : node.Id;
                 };
 
-            using (var writer = new StreamWriter(path))
+            var transformationModule = Model.Presentation.GetModule<ITransformationModule>();
+            using( var writer = new StreamWriter( path ) )
             {
-                DgmlExporter.Export(Model.Presentation.Graph, GetNodeCaption, writer);
+                DgmlExporter.Export( transformationModule.Graph, GetNodeCaption, writer );
             }
         }
 
-        private void SaveAsDot(string path)
+        private void SaveAsDot( string path )
         {
-            var writer = new DotWriter(path);
-            writer.Write(Model.Presentation);
+            var writer = new DotWriter( path );
+            writer.Write( Model.Presentation );
         }
 
-        protected override void OnModelPropertyChanged(string propertyName)
+        protected override void OnModelPropertyChanged( string propertyName )
         {
-            PropertyChangedEventManager.AddHandler(Model, OnPresentationChanged, PropertySupport.ExtractPropertyName(() => Model.Presentation));
+            PropertyChangedEventManager.AddHandler( Model, OnPresentationChanged, PropertySupport.ExtractPropertyName( () => Model.Presentation ) );
         }
 
-        private void OnPresentationChanged(object sender, PropertyChangedEventArgs e)
+        private void OnPresentationChanged( object sender, PropertyChangedEventArgs e )
         {
             SaveDocumentCommand.RaiseCanExecuteChanged();
         }
