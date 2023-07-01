e80805a7d454640239d5b603afaa1449fdc0af4d
Daniel Günzel <daniel.guenzel.ext@siemens-healthineers.com>
2023-07-01 21:00:18 +0200

    Added simple parser test.

---
+++ src/Plainion.GraphViz.Modules.MdFiles.Tests/ParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Abstractions;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Plainion.GraphViz.Modules.MdFiles.Tests
+{
+    [TestFixture]
+    internal class ParserTests
+    {
+        [Test]
+        public void Parse_MarkDownDocument()
+        {
+            var parser = new Dependencies.Parser.MarkdigParser(new FileSystem());
+
+            var md = parser.LoadMarkdown("[Introduction](UserManual/Introduction.md)");
+
+            Assert.IsTrue(md.Links.Any());
+        }
+    }
+}
