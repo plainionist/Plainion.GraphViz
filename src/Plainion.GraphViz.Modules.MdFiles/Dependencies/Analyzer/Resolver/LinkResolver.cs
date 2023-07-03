117f9722b9035ee380c4671d99fb9b33938e8224
Daniel Günzel <daniel.guenzel.ext@siemens-healthineers.com>
2023-07-03 19:00:19 +0200

    Refactoring.

--- src/Plainion.GraphViz.Modules.MdFiles/Dependencies/Analyzer/Resolver/LinkResolver.cs
+++ src/Plainion.GraphViz.Modules.MdFiles/Dependencies/Analyzer/Resolver/LinkResolver.cs
@@ -18,12 +18,20 @@ namespace Plainion.GraphViz.Modules.MdFiles.Dependencies.Analyzer.Resolver
         private static bool IsExternalLink(string url)
         {
             return Uri.TryCreate(url, UriKind.Absolute, out _)
-                || url.StartsWith("\\"); // Workaround: UNC paths are not supported by MD
+                || IsUncPath(url);
+        }
+
+        private static bool IsUncPath(string url)
+        {
+            // Workaround: UNC paths are not supported by MD and are not parsed correctly (doubles backslashes are not escaped).
+            // However, since they might appear in the document, we want to correctly specify them as an external link.
+            return url.StartsWith("\\");
         }
 
         private static ResolvedLink Resolve(string url, string currentDir, string root)
         {
-            // Remove leading slash otherwise all subdirectories are lost in the new path.
+            // Remove leading slash of the url otherwise all subdirectories of the currentDir are lost in the new path,
+            // e.g. ("/Folder C/document.md", "C:\Folder A\Folder B\") becomes to "C:\Folder C\document.md".
             var path = Path.GetFullPath(url.TrimStart('/'), currentDir);
 
             if (IsOutsideRoot(path, root))
