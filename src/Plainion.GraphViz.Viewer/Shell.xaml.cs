6b91f0b9373cc8720dacf20df07eeb0ab474bf04
plainionist <stanley751@gmx.net>
2023-07-25 17:47:33 +0200

    Simple full screen mode

--- src/Plainion.GraphViz.Viewer/Shell.xaml.cs
+++ src/Plainion.GraphViz.Viewer/Shell.xaml.cs
@@ -1,22 +1,45 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Input;
 
 namespace Plainion.GraphViz.Viewer
 {
     partial class Shell : Window
     {
-        public Shell( ShellViewModel model )
+        public Shell(ShellViewModel model)
         {
             InitializeComponent();
 
             DataContext = model;
         }
 
-        private void OnTipsClick( object sender, RoutedEventArgs e )
+        private void OnTipsClick(object sender, RoutedEventArgs e)
         {
-            var tooltip = ( ( ToolTip )( ( Hyperlink )sender ).ToolTip );
+            var tooltip = ((ToolTip)((Hyperlink)sender).ToolTip);
             tooltip.IsOpen = !tooltip.IsOpen;
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.Key == Key.F11)
+            {
+                if (WindowStyle == WindowStyle.None)
+                {
+                    WindowStyle = WindowStyle.SingleBorderWindow;
+                    ResizeMode = ResizeMode.CanResize;
+                    WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    WindowStyle = WindowStyle.None;
+                    ResizeMode = ResizeMode.NoResize;
+                    WindowState = WindowState.Maximized;
+                }
+
+                e.Handled = true;
+            }
+            base.OnKeyDown(e);
         }
     }
 }
