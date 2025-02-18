9717124bc0be4c8084c9777a720aea5a61d3abe1
plainionist <stanley751@gmx.net>
2025-02-18 09:39:32 +0100

    code cleanup

--- src/Plainion.GraphViz/GraphPaginator.cs
+++ src/Plainion.GraphViz/GraphPaginator.cs
@@ -16,46 +16,46 @@ namespace Plainion.GraphViz
         private int myPageCountX;  // Size of the printing in page units
         private int myPageCountY;
 
-        public GraphPaginator( DrawingVisual source, Size printSize )
+        public GraphPaginator(DrawingVisual source, Size printSize)
         {
             PageSize = printSize;
 
-            contentSize = new Size( printSize.Width - 2 * Margin, printSize.Height - 2 * Margin );
+            contentSize = new Size(printSize.Width - 2 * Margin, printSize.Height - 2 * Margin);
 
-            myFrameRect = new Rect( new Point( Margin, Margin ), contentSize );
-            myFrameRect.Inflate( 1, 1 );
-            myFramePen = new Pen( Brushes.Black, 0.1 );
+            myFrameRect = new Rect(new Point(Margin, Margin), contentSize);
+            myFrameRect.Inflate(1, 1);
+            myFramePen = new Pen(Brushes.Black, 0.1);
 
             // Transformation to borderless print size
             var bounds = source.DescendantBounds;
-            bounds.Union( source.ContentBounds );
+            bounds.Union(source.ContentBounds);
 
             var m = new Matrix();
-            m.Translate( -bounds.Left, -bounds.Top );
+            m.Translate(-bounds.Left, -bounds.Top);
 
             double scale = 16; // hardcoded zoom for printing
-            myPageCountX = ( int )( ( bounds.Width * scale ) / contentSize.Width ) + 1;
-            myPageCountY = ( int )( ( bounds.Height * scale ) / contentSize.Height ) + 1;
-            m.Scale( scale, scale );
-        
+            myPageCountX = (int)((bounds.Width * scale) / contentSize.Width) + 1;
+            myPageCountY = (int)((bounds.Height * scale) / contentSize.Height) + 1;
+            m.Scale(scale, scale);
+
             // Center on available pages
-            m.Translate( ( myPageCountX * contentSize.Width - bounds.Width * scale ) / 2, ( myPageCountY * contentSize.Height - bounds.Height * scale ) / 2 );
+            m.Translate((myPageCountX * contentSize.Width - bounds.Width * scale) / 2, (myPageCountY * contentSize.Height - bounds.Height * scale) / 2);
 
             var target = new DrawingVisual();
-            using( var dc = target.RenderOpen() )
+            using (var dc = target.RenderOpen())
             {
-                dc.PushTransform( new MatrixTransform( m ) );
-                dc.DrawDrawing( source.Drawing );
+                dc.PushTransform(new MatrixTransform(m));
+                dc.DrawDrawing(source.Drawing);
 
-                foreach( DrawingVisual child in source.Children )
+                foreach (DrawingVisual child in source.Children)
                 {
-                    dc.DrawDrawing( child.Drawing );
+                    dc.DrawDrawing(child.Drawing);
                 }
             }
             myGraph = target.Drawing;
         }
 
-        public override DocumentPage GetPage( int pageNumber )
+        public override DocumentPage GetPage(int pageNumber)
         {
             int x = pageNumber % myPageCountX;
             int y = pageNumber / myPageCountX;
@@ -66,43 +66,24 @@ namespace Plainion.GraphViz
             view.Size = contentSize;
 
             var targetVisual = new DrawingVisual();
-            using( var dc = targetVisual.RenderOpen() )
+            using (var dc = targetVisual.RenderOpen())
             {
-                dc.DrawRectangle( null, myFramePen, myFrameRect );
-                dc.PushTransform( new TranslateTransform( Margin - view.X, Margin - view.Y ) );
-                dc.PushClip( new RectangleGeometry( view ) );
-               
-                dc.DrawDrawing( myGraph );
+                dc.DrawRectangle(null, myFramePen, myFrameRect);
+                dc.PushTransform(new TranslateTransform(Margin - view.X, Margin - view.Y));
+                dc.PushClip(new RectangleGeometry(view));
+
+                dc.DrawDrawing(myGraph);
             }
 
-            return new DocumentPage( targetVisual, PageSize, myFrameRect, myFrameRect );
+            return new DocumentPage(targetVisual, PageSize, myFrameRect, myFrameRect);
         }
 
-        public override bool IsPageCountValid
-        {
-            get
-            {
-                return true;
-            }
-        }
+        public override bool IsPageCountValid => true;
 
-        public override int PageCount
-        {
-            get
-            {
-                return myPageCountY * myPageCountX;
-            }
-        }
+        public override int PageCount => myPageCountY * myPageCountX;
 
-        public override Size PageSize
-        {
-            get;
-            set;
-        }
+        public override Size PageSize { get; set; }
 
-        public override IDocumentPaginatorSource Source
-        {
-            get { return null; }
-        }
+        public override IDocumentPaginatorSource Source => null;
     }
 }
