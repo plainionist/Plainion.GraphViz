d375aa1152664cb19cecadbff3ea8fa6394a0f76
plainionist <stanley751@gmx.net>
2025-02-21 21:02:37 +0100

    SpecialGraphBuilder extracted

--- src/Plainion.GraphViz.Modules.CodeInspection/AssemblyDependencyAnalyzer.cs
+++ src/Plainion.GraphViz.Modules.CodeInspection/AssemblyDependencyAnalyzer.cs
@@ -5,7 +5,6 @@ using System.Reflection;
 using Plainion.GraphViz.Algorithms;
 using Plainion.GraphViz.CodeInspection;
 using Plainion.GraphViz.CodeInspection.AssemblyLoader;
-using Plainion.GraphViz.Model;
 using Plainion.GraphViz.Presentation;
 using Plainion.Text;
 
@@ -36,7 +35,11 @@ public class AssemblyDependencyAnalyzer
     {
         var deps = sources.Aggregate(new List<AssemblyReference>(), (acc, asm) => Analyze(acc, asm, false));
 
-        return new GraphBuilder().CreateAssemblyRelationshipGraph(sources.Select(R.AssemblyName).ToList(), targets.Select(R.AssemblyName).ToList(), deps.ToList());
+        return new SpecialGraphBuilder()
+            .CreateGraphOfReachables(
+                sources.Select(R.AssemblyName),
+                targets.Select(R.AssemblyName),
+                deps.Select(r => (R.AssemblyName(r.myAssembly), R.AssemblyName(r.myDependency))));
     }
 
     private List<AssemblyReference> Analyze(List<AssemblyReference> analyzed, Assembly asm, bool isSource)
@@ -71,51 +74,5 @@ public class AssemblyDependencyAnalyzer
     private bool FollowAssembly(Assembly asm)
     {
         return myRelevantAssemblies.Any(p => p.IsMatch(asm.GetName().Name));
-    }
-
-    private class GraphBuilder
-    {
-        public GraphPresentation CreateAssemblyRelationshipGraph(IReadOnlyCollection<string> sourceNodes, IReadOnlyCollection<string> targetNodes, IEnumerable<AssemblyReference> assemblyReferences)
-        {
-            var builder = new RelaxedGraphBuilder();
-
-            var edges = assemblyReferences
-                .Select(r => (R.AssemblyName(r.myAssembly), R.AssemblyName(r.myDependency)))
-                .Where(x => x.Item1 != x.Item2);
-
-            foreach (var (f, t) in edges)
-            {
-                builder.TryAddEdge(f, t);
-            }
-
-            foreach (var n in targetNodes)
-            {
-                builder.TryAddNode(n);
-            }
-
-            var targetCluster = builder.TryAddCluster("TARGET", targetNodes);
-            builder.TryAddCluster("SOURCE", sourceNodes);
-
-            return ReduceGraph(targetCluster, builder);
-        }
-
-        // Show only nodes which can reach the target cluster
-        private GraphPresentation ReduceGraph(Cluster targetCluster, RelaxedGraphBuilder builder)
-        {
-            var presentation = new GraphPresentation();
-            presentation.Graph = builder.Graph;
-
-            var algo = new AddRemoveTransitiveHull(presentation);
-            algo.Add = false;
-            algo.Reverse = true;
-            var mask = algo.Compute(targetCluster.Nodes);
-            mask.Invert(presentation);
-
-            presentation.Masks().Push(mask);
-
-            presentation.Masks().Push(new RemoveNodesWithoutSiblings(presentation).Compute());
-
-            return presentation;
-        }
     }
 }
