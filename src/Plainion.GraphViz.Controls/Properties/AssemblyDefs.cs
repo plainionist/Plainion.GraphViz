826683f45aaeebff374f0e6a4d442bcd707ba6a6
plainionist <stanley751@gmx.net>
2025-02-22 17:38:50 +0100

    XML namespace aligned with project

--- src/Plainion.GraphViz.Controls/Properties/AssemblyDefs.cs
+++ src/Plainion.GraphViz.Controls/Properties/AssemblyDefs.cs
@@ -1,7 +1,8 @@
 ﻿using System.Runtime.Versioning;
 using System.Windows.Markup;
 
-[assembly: XmlnsPrefix("http://github.com/ronin4net/plainion", "pn")]
-[assembly: XmlnsDefinition("http://github.com/ronin4net/plainion", "Plainion.GraphViz.Controls.XmlEditor")]
+[assembly: XmlnsPrefix("https://github.com/plainionist/Plainion.GraphViz", "pngv")]
+[assembly: XmlnsDefinition("https://github.com/plainionist/Plainion.GraphViz", "Plainion.GraphViz.Controls.XmlEditor")]
+[assembly: XmlnsDefinition("https://github.com/plainionist/Plainion.GraphViz", "Plainion.GraphViz.Controls.Tree")]
 
 [assembly: SupportedOSPlatform("windows7.0")]
