3d4cafb124e77a83fee0edd56ccac14f24a78973
plainionist <stanley751@gmx.net>
2025-02-23 09:48:54 +0100

    DragDropBehavior simplified

--- src/Plainion.GraphViz.Modules.Analysis/Clusters/DragDropBehavior.cs
+++ src/Plainion.GraphViz.Modules.Analysis/Clusters/DragDropBehavior.cs
@@ -1,128 +1,34 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using Plainion.Collections;
-using Plainion.Windows.Interactivity.DragDrop;
+﻿namespace Plainion.GraphViz.Modules.Analysis.Clusters;
 
-namespace Plainion.GraphViz.Modules.Analysis.Clusters;
-
-/// <summary>
-/// Implements DragDrop in the tree. Can be used by users of the TreeEditor to handle 
-/// Drag and Drop commands.
-/// <para>
-/// Pre-conditions:
-/// - INode.Parent must be writable
-/// - INode.Children must be of type ObservableCollection{T}
-/// </para>
-/// </summary>
 class DragDropBehavior
 {
-    private NodeWriteAccess myRoot;
-
-    private class ObservableCollection
-    {
-        private IEnumerable<NodeViewModel> myCollection;
-
-        public ObservableCollection(IEnumerable<NodeViewModel> collection)
-        {
-            Contract.Requires(collection.GetType().IsGenericType
-                && collection.GetType().GetGenericTypeDefinition() == typeof(ObservableCollection<>), "Collection is not of type ObservableCollection<T>");
-
-            myCollection = collection;
-        }
-
-        public void Add(NodeWriteAccess node)
-        {
-            ((IList)myCollection).Add(node.Node);
-        }
-
-        public void Insert(int pos, NodeWriteAccess node)
-        {
-            ((IList)myCollection).Insert(pos, node.Node);
-        }
-
-        public void Move(int oldPos, int newPos)
-        {
-            myCollection.GetType().GetMethod("Move").Invoke(myCollection, new object[] { oldPos, newPos });
-        }
-
-        public void Remove(NodeWriteAccess node)
-        {
-            ((IList)myCollection).Remove(node.Node);
-        }
-
-        public int IndexOf(NodeWriteAccess node)
-        {
-            return myCollection.IndexOf(node.Node);
-        }
-
-        public bool Contains(NodeWriteAccess node)
-        {
-            return myCollection.Contains(node.Node);
-        }
-
-        public int Count { get { return myCollection.Count(); } }
-    }
-
-    private class NodeWriteAccess
-    {
-        public NodeWriteAccess(NodeViewModel node)
-        {
-            Contract.RequiresNotNull(node, "node");
-
-            Node = node;
-
-            Contract.Requires(Node.GetType().GetProperty(nameof(Node.Parent)).CanWrite, "Parent is not writable");
-
-            Children = new ObservableCollection(Node.Children);
-        }
-
-        public NodeViewModel Node { get; private set; }
-
-        public ObservableCollection Children { get; private set; }
-
-        public NodeWriteAccess GetParent()
-        {
-            return Node.Parent != null ? new NodeWriteAccess(Node.Parent) : null;
-        }
-
-        public void SetParent(NodeViewModel node)
-        {
-            Node.GetType().GetProperty(nameof(Node.Parent)).SetValue(Node, node);
-        }
-    }
+    private readonly NodeViewModel myRoot;
 
     public DragDropBehavior(NodeViewModel root)
     {
         System.Contract.RequiresNotNull(root);
-
-        myRoot = new NodeWriteAccess(root);
+        myRoot = root;
     }
 
     public void ApplyDrop(NodeDropRequest request)
     {
-        var droppedNode = new NodeWriteAccess(request.DroppedNode);
-        var dropTarget = new NodeWriteAccess(request.DropTarget);
-
-        if (request.DropTarget == myRoot.Node)
+        if (request.DropTarget == myRoot)
         {
-            var oldParent = droppedNode.GetParent();
-            oldParent.Children.Remove(droppedNode);
+            var oldParent = request.DroppedNode.Parent;
+            oldParent.Children.Remove(request.DroppedNode);
 
-            myRoot.Children.Add(droppedNode);
+            myRoot.Children.Add(request.DroppedNode);
 
-            droppedNode.SetParent(myRoot.Node);
+            request.DroppedNode.Parent = myRoot;
         }
         else
         {
-            var oldParent = droppedNode.GetParent();
-            oldParent.Children.Remove(droppedNode);
+            var oldParent = request.DroppedNode.Parent;
+            oldParent.Children.Remove(request.DroppedNode);
 
-            dropTarget.Children.Add(droppedNode);
+            request.DropTarget.Children.Add(request.DroppedNode);
 
-            droppedNode.SetParent(dropTarget.Node);
+            request.DroppedNode.Parent = request.DropTarget;
         }
     }
 }
