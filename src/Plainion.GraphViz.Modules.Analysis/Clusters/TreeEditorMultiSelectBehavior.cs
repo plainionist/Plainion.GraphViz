a5d65a69d97ff77966fb4f31c2c4c15755946cf5
plainionist <stanley751@gmx.net>
2025-02-25 16:00:18 +0100

    TreeEditorMultiSelectBehavior: shift select support added

--- src/Plainion.GraphViz.Modules.Analysis/Clusters/TreeEditorMultiSelectBehavior.cs
+++ src/Plainion.GraphViz.Modules.Analysis/Clusters/TreeEditorMultiSelectBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -6,15 +7,17 @@ using Microsoft.Xaml.Behaviors;
 
 namespace Plainion.GraphViz.Modules.Analysis.Clusters;
 
-public class TreeEditorMultiSelectBehavior : Behavior<TreeView>
+class TreeEditorMultiSelectBehavior : Behavior<TreeView>
 {
+    private NodeView myLastSelectedItem;
+
     public static readonly DependencyProperty SelectedItemsProperty =
-        DependencyProperty.Register(nameof(SelectedItems), typeof(IList), typeof(TreeEditorMultiSelectBehavior),
+        DependencyProperty.Register(nameof(SelectedItems), typeof(IList<NodeViewModel>), typeof(TreeEditorMultiSelectBehavior),
             new PropertyMetadata(null));
 
-    public IList SelectedItems
+    public IList<NodeViewModel> SelectedItems
     {
-        get => (IList)GetValue(SelectedItemsProperty);
+        get => (IList<NodeViewModel>)GetValue(SelectedItemsProperty);
         set => SetValue(SelectedItemsProperty, value);
     }
 
@@ -42,7 +45,7 @@ public class TreeEditorMultiSelectBehavior : Behavior<TreeView>
             return;
         }
 
-        var item = source.FindParentOfType<TreeViewItem>();
+        var item = source.FindParentOfType<NodeView>();
         if (item == null || item.DataContext == null)
         {
             return;
@@ -53,26 +56,64 @@ public class TreeEditorMultiSelectBehavior : Behavior<TreeView>
             // Toggle selection
             if (SelectedItems.Contains(item.DataContext))
             {
-                SelectedItems.Remove(item.DataContext);
+                SelectedItems.Remove((NodeViewModel)item.DataContext);
+                item.IsSelected = false;
             }
             else
             {
-                SelectedItems.Add(item.DataContext);
+                SelectedItems.Add((NodeViewModel)item.DataContext);
+                item.IsSelected = true;
             }
         }
         else if (Keyboard.Modifiers == ModifierKeys.Shift && SelectedItems.Count > 0)
         {
-            // Range selection (optional, requires more logic for hierarchy)
-            // For simplicity, we'll skip full range logic here
+            var items = GetTreeViewItemRange(source.FindParentOfType<ItemsControl>(), myLastSelectedItem, item);
+            foreach (var x in items)
+            {
+                SelectedItems.Add((NodeViewModel)x.DataContext);
+                x.IsSelected = true;
+            }
         }
         else
         {
             // Single click clears and selects
+            foreach (var x in SelectedItems)
+            {
+                x.IsSelected = false;
+            }
             SelectedItems.Clear();
-            SelectedItems.Add(item.DataContext);
+            SelectedItems.Add((NodeViewModel)item.DataContext);
+            item.IsSelected = true;
+            myLastSelectedItem = item;
         }
 
         // allow default behavior in parallel
-        // e.Handled = true;
+        e.Handled = true;
+    }
+
+    private List<NodeView> GetTreeViewItemRange(ItemsControl parentItem, NodeView start, NodeView end)
+    {
+        var items = new List<NodeView>();
+
+        for (var index = 0; index < parentItem.Items.Count; index++)
+        {
+            var tvItem = parentItem.ItemContainerGenerator.ContainerFromIndex(index) as NodeView;
+            if (tvItem == null) continue;
+
+            items.Add(tvItem);
+        }
+
+        var startIndex = items.IndexOf(start);
+        var endIndex = items.IndexOf(end);
+        var rangeStart = startIndex > endIndex || startIndex == -1 ? endIndex : startIndex;
+        var rangeCount = startIndex > endIndex ? startIndex - endIndex + 1 : endIndex - startIndex + 1;
+
+        if (startIndex == -1 && endIndex == -1)
+            rangeCount = 0;
+
+        else if (startIndex == -1 || endIndex == -1)
+            rangeCount = 1;
+
+        return rangeCount > 0 ? items.GetRange(rangeStart, rangeCount) : new List<NodeView>();
     }
 }
