3ef5ea6799fdbe557056ed4e989f789e7a526106
plainionist <stanley751@gmx.net>
2025-03-02 21:50:35 +0100

    Plainion.Graphs.Algorithms extracted

---
+++ src/Plainion.Graphs.Algorithms/Traverse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Plainion.Graphs;
+
+namespace Plainion.GraphViz.Algorithms
+{
+    class Traverse
+    {
+        public static IEnumerable<Edge> BreathFirst(IEnumerable<Node> roots, Func<Node, IEnumerable<Edge>> siblingsSelector)
+        {
+            var unvisitedNodes = new HashSet<Node>(roots);
+            var visitedNodes = new HashSet<Node>();
+
+            while (unvisitedNodes.Count > 0)
+            {
+                var currentNodes = unvisitedNodes.ToList();
+                unvisitedNodes.Clear();
+
+                foreach (var node in currentNodes)
+                {
+                    foreach (var edge in siblingsSelector(node))
+                    {
+                        unvisitedNodes.Add(edge.Source);
+                        unvisitedNodes.Add(edge.Target);
+
+                        yield return edge;
+                    }
+
+                    visitedNodes.Add(node);
+                }
+
+                unvisitedNodes.ExceptWith(visitedNodes);
+            }
+        }
+    }
+}
