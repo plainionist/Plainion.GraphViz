3670edd267f1c3e45c55bc4d01e3cf808fdde500
plainionist <stanley751@gmx.net>
2025-03-03 20:46:56 +0100

    Metrics: shortest path finder added

---
+++ src/Plainion.GraphViz.Modules.Metrics.Tests/ShortestPathsFinderTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Plainion.Graphs;
+
+
+namespace Plainion.GraphViz.Modules.Metrics.Tests;
+
+[TestFixture]
+public class ShortestPathsFinderTests
+{
+    [Test]
+    public void NoEdges_ReturnsNoPaths()
+    {
+        var builder = new RelaxedGraphBuilder();
+        builder.TryAddEdge("A", "B");
+        var graph = builder.Graph;
+
+        var result = ShortestPathsFinder.FindAllShortestPaths(graph);
+
+        var abPath = result.Paths.FirstOrDefault(p => p[0].Source.Id == "A" && p.Last().Target.Id == "B");
+        Assert.That(abPath, Is.Not.Null);
+        Assert.That(abPath.Count, Is.EqualTo(1));
+        Assert.That(abPath[0].Source.Id, Is.EqualTo("A"));
+        Assert.That(abPath[0].Target.Id, Is.EqualTo("B"));
+        Assert.That(ShortestPathsResult.GetDistance(abPath), Is.EqualTo(1));
+
+        var bPaths = result.Paths.Any(p => p[0].Source.Id == "B");
+        Assert.That(bPaths, Is.False); // B has no outgoing paths
+    }
+
+    [Test]
+    public void TriangleGraph_ReturnsAllPaths()
+    {
+        var builder = new RelaxedGraphBuilder();
+        builder.TryAddEdge("A", "B");
+        builder.TryAddEdge("B", "C");
+        builder.TryAddEdge("C", "A");
+
+        var result = ShortestPathsFinder.FindAllShortestPaths(builder.Graph);
+
+        // A -> B -> C
+        var ab = result.Paths.First(p => p[0].Source.Id == "A" && p.Last().Target.Id == "B");
+        Assert.That(ab.Count, Is.EqualTo(1));
+        Assert.That(ShortestPathsResult.GetDistance(ab), Is.EqualTo(1));
+        var ac = result.Paths.First(p => p[0].Source.Id == "A" && p.Last().Target.Id == "C");
+        Assert.That(ac.Count, Is.EqualTo(2));
+        Assert.That(ShortestPathsResult.GetDistance(ac), Is.EqualTo(2));
+
+        // B -> C -> A
+        var bc = result.Paths.First(p => p[0].Source.Id == "B" && p.Last().Target.Id == "C");
+        Assert.That(bc.Count, Is.EqualTo(1));
+        Assert.That(ShortestPathsResult.GetDistance(bc), Is.EqualTo(1));
+        var ba = result.Paths.First(p => p[0].Source.Id == "B" && p.Last().Target.Id == "A");
+        Assert.That(ba.Count, Is.EqualTo(2));
+        Assert.That(ShortestPathsResult.GetDistance(ba), Is.EqualTo(2));
+
+        // C -> A -> B
+        var ca = result.Paths.First(p => p[0].Source.Id == "C" && p.Last().Target.Id == "A");
+        Assert.That(ca.Count, Is.EqualTo(1));
+        Assert.That(ShortestPathsResult.GetDistance(ca), Is.EqualTo(1));
+        var cb = result.Paths.First(p => p[0].Source.Id == "C" && p.Last().Target.Id == "B");
+        Assert.That(cb.Count, Is.EqualTo(2));
+        Assert.That(ShortestPathsResult.GetDistance(cb), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void DisconnectedGraph_ReturnsPartialPaths()
+    {
+        var builder = new RelaxedGraphBuilder();
+        builder.TryAddEdge("A", "B");
+        builder.TryAddEdge("C", "D");
+
+        var result = ShortestPathsFinder.FindAllShortestPaths(builder.Graph);
+
+        var ab = result.Paths.FirstOrDefault(p => p[0].Source.Id == "A" && p.Last().Target.Id == "B");
+        Assert.That(ab, Is.Not.Null);
+        Assert.That(ShortestPathsResult.GetDistance(ab), Is.EqualTo(1));
+
+        var cd = result.Paths.FirstOrDefault(p => p[0].Source.Id == "C" && p.Last().Target.Id == "D");
+        Assert.That(cd, Is.Not.Null);
+        Assert.That(ShortestPathsResult.GetDistance(cd), Is.EqualTo(1));
+
+        var ac = result.Paths.Any(p => p[0].Source.Id == "A" && p.Last().Target.Id == "C");
+        Assert.That(ac, Is.False);
+    }
+
+    [Test]
+    public void WeightedEdges_ReturnsCorrectDistances()
+    {
+        var builder = new RelaxedGraphBuilder();
+        builder.TryAddEdge("A", "B");
+        builder.TryAddEdge("B", "C");
+        builder.TryAddEdge("A", "C", 3);
+
+        var result = ShortestPathsFinder.FindAllShortestPaths(builder.Graph);
+
+        var ab = result.Paths.First(p => p[0].Source.Id == "A" && p.Last().Target.Id == "B");
+        Assert.That(ShortestPathsResult.GetDistance(ab), Is.EqualTo(1));
+        var ac = result.Paths.First(p => p[0].Source.Id == "A" && p.Last().Target.Id == "C");
+        Assert.That(ac.Count, Is.EqualTo(2)); // A -> B -> C
+        Assert.That(ShortestPathsResult.GetDistance(ac), Is.EqualTo(2)); // Shorter than direct A -> C (3)
+    }
+}
---
+++ src/Plainion.GraphViz.Modules.Metrics/ShortestPathFinder.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Plainion.Graphs;
+
+namespace Plainion.GraphViz.Modules.Metrics;
+
+public class ShortestPathsResult
+{
+    public List<List<Edge>> Paths { get; }
+
+    public ShortestPathsResult(List<List<Edge>> paths)
+    {
+        Paths = paths;
+    }
+
+    public static int GetDistance(List<Edge> path) => path.Sum(e => e.Weight);
+}
+
+public static class ShortestPathsFinder
+{
+    public static ShortestPathsResult FindAllShortestPaths(IGraph graph)
+    {
+        var allPaths = new List<List<Edge>>();
+        var lockObj = new object();
+
+        Parallel.ForEach(graph.Nodes, source =>
+        {
+            var (dist, prev, edges) = Dijkstra(graph, source);
+            var sourcePaths = new List<List<Edge>>();
+
+            foreach (var target in graph.Nodes)
+            {
+                if (target.Id != source.Id && dist.TryGetValue(target.Id, out int distance) && distance != int.MaxValue)
+                {
+                    var path = ReconstructPath(source, target, prev, edges);
+                    if (path.Count > 0) // Only add non-empty paths
+                        sourcePaths.Add(path);
+                }
+            }
+
+            lock (lockObj)
+            {
+                allPaths.AddRange(sourcePaths);
+            }
+        });
+
+        return new ShortestPathsResult(allPaths);
+    }
+
+    private static (Dictionary<string, int> dist, Dictionary<string, string> prev, Dictionary<string, Edge> edges) Dijkstra(IGraph graph, Node source)
+    {
+        var dist = graph.Nodes.ToDictionary(n => n.Id, _ => int.MaxValue);
+        var prev = new Dictionary<string, string>();
+        var edges = new Dictionary<string, Edge>(); // Tracks edge leading to each node
+        var pq = new PriorityQueue<string, int>();
+
+        dist[source.Id] = 0;
+        pq.Enqueue(source.Id, 0);
+
+        while (pq.Count > 0)
+        {
+            var u = pq.Dequeue();
+            var uNode = graph.FindNode(u);
+
+            foreach (var edge in uNode.Out)
+            {
+                var v = edge.Target.Id;
+                int alt = dist[u] == int.MaxValue ? int.MaxValue : dist[u] + edge.Weight;
+
+                if (alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = u;
+                    edges[v] = edge; // Store the edge used to reach v
+                    pq.Enqueue(v, alt);
+                }
+            }
+        }
+
+        return (dist, prev, edges);
+    }
+
+    private static List<Edge> ReconstructPath(Node start, Node end, Dictionary<string, string> prev, Dictionary<string, Edge> edges)
+    {
+        var path = new List<Edge>();
+        var currentId = end.Id;
+
+        while (prev.ContainsKey(currentId))
+        {
+            var prevId = prev[currentId];
+            if (edges.TryGetValue(currentId, out var edge))
+            {
+                path.Add(edge);
+            }
+            currentId = prevId;
+            if (currentId == start.Id) break;
+        }
+
+        path.Reverse();
+        return path.Count > 0 && path[0].Source.Id == start.Id ? path : new List<Edge>();
+    }
+}
+
